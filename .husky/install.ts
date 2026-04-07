f2f12a67b35bb008ae050065343c54e76eac70f1
Jesús Iván Godínez Martínez <ivano.godinez@gmail.com>
2026-04-06 20:28:29 -0600

    fix: husky install

--- .husky/install.ts
+++ .husky/install.ts
@@ -1,7 +1,9 @@
-import husky from 'husky';
-
-if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
+if (process.env.NODE_ENV === 'production' || process.env.CI) {
   process.exit(0);
 }
 
+const { default: husky } = await import('husky');
+
 console.log(husky());
+
+export {};
